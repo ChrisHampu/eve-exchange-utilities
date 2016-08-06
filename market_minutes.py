38b2f5f87d575640ab003bf12fbf8dec1abaa0ca
Chris Hampu <chrishampu@outlook.com>
2016-08-06 16:15:01 -0700

    Fix daily check triggering hourly operations instead

--- market_minutes.py
+++ market_minutes.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
   # 11 AM UTC (EVE downtime)
   if (utt.tm_hour == 11 and utt.tm_min == 00):
     print("Writing daily data")
-    useHourly = True 
+    useDaily = True 
 
   req = requests.get("https://crest-tq.eveonline.com/market/10000002/orders/all/")
 
