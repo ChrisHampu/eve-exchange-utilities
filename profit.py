af3e8437f7e20ab054c8094458e300d7c3029241
Chris Hampu <chrishampu@outlook.com>
2016-08-23 02:43:56 -0700

    All time profit tracking, transaction records, bug fixes

--- profit.py
+++ profit.py
@@ -1,7 +1,7 @@
 import sys
 import requests
 import time
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, tzinfo
 import rethinkdb as r
 
 try:
@@ -19,6 +19,8 @@ HorizonDB = sys.argv[1]
 SettingsTable = sys.argv[2]
 ProfitChartTable = sys.argv[3]
 TopitemsTable = sys.argv[4]
+AlltimeTable = sys.argv[5]
+TransactionsTable = sys.argv[6]
 rowCount = 1000
 
 def getTransactions(fromID=None):
@@ -29,13 +31,19 @@ def getTransactions(fromID=None):
 
   req = requests.get(url)
 
-  tree = ET.fromstring(req.text)
+  rows = []
 
-  if tree.find('error') is not None:
-    print("Error while pulling transactions")
-    return []
+  try:
+    tree = ET.fromstring(req.text)
 
-  rows = list(tree.find('result').find('rowset'))
+    if tree.find('error') is not None:
+      print("Error while pulling transactions")
+      return []
+
+    rows = list(tree.find('result').find('rowset'))
+
+  except:
+    pass
 
   return rows
 
@@ -43,6 +51,10 @@ def getAllTransactions():
 
   fromID = None
   journal = getTransactions(fromID)
+
+  if len(journal) == 0:
+    return []
+
   fromID = journal[-1].attrib['transactionID']
   lastPull = len(journal)
   journal[0].attrib['transactionID']
@@ -50,6 +62,8 @@ def getAllTransactions():
   while lastPull >= rowCount-1:
     newJournal = getTransactions(fromID)
     lastPull = len(newJournal)
+    if lastPull == 0:
+      break
     journal.extend(newJournal)
     fromID = newJournal[-1].attrib['transactionID']
 
@@ -67,13 +81,18 @@ def getJournalEntries():
 
   req = requests.get(url)
 
-  tree = ET.fromstring(req.text)
+  rows = []
 
-  if tree.find('error') is not None:
-    print("Error while pulling transactions")
-    return []
+  try:
+    tree = ET.fromstring(req.text)
 
-  rows = list(tree.find('result').find('rowset'))
+    if tree.find('error') is not None:
+      print("Error while pulling transactions")
+      return []
+
+    rows = list(tree.find('result').find('rowset'))
+  except:
+    pass
 
   data = [{key:row.attrib[key] for key in ('amount', 'refTypeID', 'date')} for row in rows if time.strptime(row.attrib['date'], '%Y-%m-%d %H:%M:%S') > offset]
 
@@ -91,10 +110,12 @@ if __name__ == '__main__':
   dt = datetime.utcnow() - timedelta(hours=1)
   offset = dt.utctimetuple()
 
-  settings = r.table(SettingsTable).run(horizon_conn)
+  settings = list(r.table(SettingsTable).run(horizon_conn))
 
   results = []
+  transactions = []
   salesResults = dict()
+  profitResults = dict()
 
   for user in settings:
     
@@ -133,7 +154,7 @@ if __name__ == '__main__':
         profit = sum([float(x['price']) - float(buy['price']) for x in groupedSells[typeID]])
         totalSell = sum([float(x['price']) for x in groupedSells[typeID]])
         quantity = sum([int(x['quantity']) for x in groupedSells[typeID]])
-        
+
         sales.append({
           'type': typeID,
           'sellPrice': float(groupedSells[typeID][0]['price']),
@@ -141,7 +162,8 @@ if __name__ == '__main__':
           'totalProfit': profit,
           'quantity': quantity,
           'avgPerUnit': profit / quantity,
-          'totalSell': totalSell
+          'totalSell': totalSell,
+          'time': datetime.strptime(groupedSells[typeID][0]['transactionDateTime'], '%Y-%m-%d %H:%M:%S').replace(tzinfo=datetime.now(r.make_timezone('00:00')).tzinfo)
         })
 
       taxEntries = [x for x in journal if x['refTypeID'] == '54']
@@ -155,13 +177,12 @@ if __name__ == '__main__':
       result['broker'] = broker
       result['profit'] = totalProfit
 
-      salesResults[user['userID']] = sales;
+      salesResults[user['userID']] = sales
+      profitResults[user['userID']] = {'profit': result['profit'], 'taxes': result['taxes'], 'broker': result['broker']}
 
       results.append(result)
 
   r.table(ProfitChartTable).insert(results, durability="soft", return_changes=False, conflict="replace").run(horizon_conn)
-
-  settings = r.table(SettingsTable).run(horizon_conn)
 
   userList = [[user['userID']] for user in settings if user['eveApiKey'] is not None and len(user['eveApiKey']['keyID']) > 0]
 
@@ -180,6 +201,8 @@ if __name__ == '__main__':
       continue
 
     data = [{key:row[key] for key in ('totalProfit', 'quantity', 'avgPerUnit', 'type', 'name')} for row in sales]
+
+    transactions.extend([{**{key:row[key] for key in ('totalProfit', 'quantity', 'avgPerUnit', 'time', 'name', 'type')}, **{'userID': _charID, '$hz_v$': 0}} for row in sales])
 
     # create new top list
     if doc is None:
@@ -205,6 +228,112 @@ if __name__ == '__main__':
           doc['items'][index] = top
 
       r.table(TopitemsTable).update(doc).run(horizon_conn)
+    
+    # modify/add to alltime profit record
+    profit = profitResults[_charID] if _charID in profitResults else None
+
+    if profit is None:
+      continue
+
+    existingDoc = list(r.table(AlltimeTable).filter(lambda doc: doc['userID'] == _charID).limit(1).run(horizon_conn))
+
+    if len(existingDoc) == 0:
+      print("Creating new alltime record for user %s" % _charID)
+
+      newDoc = {
+        'userID': _charID, 
+        'alltime': profit, 
+        'day': profit, 
+        'week': profit,
+        'month': profit,
+        'biannual': profit
+      }
+
+      r.table(AlltimeTable).insert(newDoc, durability="soft", return_changes=False).run(horizon_conn)
+
+    else:
+      existingDoc = existingDoc[0]
+
+      hourlyDocs = list(r.table(ProfitChartTable).filter(lambda doc: (doc['frequency'] == "hourly") & (doc['userID'] == _charID)).order_by(r.desc("time")).limit(24).run(horizon_conn))
+
+      sumDocs = lambda docs: { 'profit': sum([i['profit'] for i in docs]), 'broker': sum([i['broker'] for i in docs]), 'taxes': sum([i['taxes'] for i in docs])}
+
+      pastDay = sumDocs(hourlyDocs)
+
+      r.table(AlltimeTable).get(existingDoc['id']).update({'alltime': { 'profit': r.row['alltime']['profit'] + profit['profit'], 'broker': r.row['alltime']['broker'] + profit['broker'], 'taxes': r.row['alltime']['taxes'] + profit['taxes'] }, 'day': pastDay}).run(horizon_conn)
+
+  r.table(TransactionsTable).insert(transactions, durability="soft", return_changes=False, conflict="replace").run(horizon_conn)
+
+  # Start daily task at 11 AM UTC (EVE downtime)
+  if nowtuple.tm_hour == 11:
+  #if True:
+
+    print("Performing daily task")
+
+    dailyDocs = []
+
+    # Aggregate hourly documents into a daily document
+    for user in settings:
+
+      userID = user['userID']
+      
+      if 'eveApiKey' not in user:
+        continue
+
+      if len(user['eveApiKey']['keyID']) and len(user['eveApiKey']['vCode']):
+
+        hourlyDocs = list(r.table(ProfitChartTable).filter(lambda doc: (doc['frequency'] == "hourly") & (doc['userID'] == userID)).order_by(r.desc("time")).limit(24).run(horizon_conn))
+
+        if len(hourlyDocs) == 0:
+          continue
+
+        dailyDoc = { 'userID': userID, 'profit': 0, 'taxes': 0, 'broker': 0, '$hz_v$': 0, 'time': now , 'frequency': 'daily' }
+
+        for doc in hourlyDocs:
+          dailyDoc['profit'] += doc['profit']
+          dailyDoc['broker'] += doc['broker']
+          dailyDoc['taxes'] += doc['taxes']
+
+        dailyDocs.append(dailyDoc)
+
+    # Insert new daily results
+    if len(dailyDocs) > 0:
+      r.table(ProfitChartTable).insert(dailyDocs, durability="soft", return_changes=False).run(horizon_conn)
+
+    # Update alltime data for each user
+    for user in settings:
+
+      userID = user['userID']
+      
+      if 'eveApiKey' not in user:
+        continue
+
+      if len(user['eveApiKey']['keyID']) and len(user['eveApiKey']['vCode']):
+
+        dailyDocs = [{'profit': i['profit'], 'broker': i['broker'], 'taxes': i['taxes']} for i in list(r.table(ProfitChartTable).filter(lambda doc: (doc['frequency'] == "daily") & (doc['userID'] == userID)).order_by(r.desc("time")).limit(90).run(horizon_conn))]
+
+        sumDocs = lambda docs: { 'profit': sum([i['profit'] for i in docs]), 'broker': sum([i['broker'] for i in docs]), 'taxes': sum([i['taxes'] for i in docs])}
+
+        pastWeek = sumDocs(dailyDocs[:7])
+        pastMonth = sumDocs(dailyDocs[:30])
+        pastBiannual = sumDocs(dailyDocs[:90])
+
+        existingDoc = list(r.table(AlltimeTable).filter(lambda doc: doc['userID'] == userID).limit(1).run(horizon_conn))
+
+        if len(existingDoc) == 0:
+          print("No alltime doc for user %s" % userID)
+          continue
+
+        else:
+          existingDoc = existingDoc[0]
+
+          r.table(AlltimeTable).get(existingDoc['id']).update({'week': pastWeek, 'month': pastMonth, 'biannual': pastBiannual}).run(horizon_conn)
+
+
+
+
+
+
 '''
 overall profit calc
   
@@ -220,5 +349,15 @@ insert to database as past 30 day report
 sum all the past 30 day reports 
 
 2. increment alltime report using past 24 hour data 
+
+structure
+{
+  "userID": id,
+  "alltime": double,
+  "24_hours": double,
+  "7_days": double,
+  "30 days": double,
+  "90 days": double
+}
 
 '''
