ea29ecd83fa77c2879964c36ea046d1f6e0ddeb6
Chris Hampu <chrishampu@outlook.com>
2016-08-25 06:59:42 -0700

    Redis test script

--- redis_test.py
+++ redis_test.py
@@ -1,31 +1,50 @@
 import redis
 import time
 
-'''
-t = time.time()
-
 re = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-re.delete("order_cache")
 
-re.hmset("order_cache", {1: t, 2: t})
+allkeys = []
 
-re.hdel("order_cache", *[2])
+idx = 0
+first = True
 
-print(re.hkeys("order_cache"))
-'''
-l =  [1, 2, 3]
+while idx != 0 or first == True:
+  keys = re.scan(match='dly:*', cursor=idx)
 
-s = set(l)
+  #print(keys)
+  idx = keys[0]
+  allkeys.extend(keys[1])
 
-print(s)
+  first = False
 
-#data = [int(k) for k in re.hgetall("order_cache")]
+print(len(allkeys))
 
-#print(data)
-#for k in data:
+t = time.perf_counter()
 
+i = 0
 
-  #print((int(k), float(data[k])))
+for k in allkeys:
+  doc = re.hmget(k, ['type', 'spread', 'tradeVolume', 'buyFifthPercentile'])
+  i += 1
 
-#print(data)
+print(i)
+print(time.perf_counter() - t)
+
+t = time.perf_counter()
+
+pip = re.pipeline()
+
+i = 0
+
+print(allkeys[0])
+
+for k in allkeys:
+  pip.hmget(k, ['type', 'spread', 'tradeVolume', 'buyFifthPercentile'])
+
+docs = pip.execute()
+
+print(docs[0])
+
+print(i)
+print(time.perf_counter() - t)
