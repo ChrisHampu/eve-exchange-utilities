ca51c8313869de95e44da8128bbfebf0222adf40
Chris Hampu <chrishampu@outlook.com>
2016-12-13 19:20:52 -0800

    Add market group parsing + item id to name lookup

--- misc/dump_daily_item.py
+++ misc/dump_daily_item.py
@@ -3,6 +3,8 @@ import csv
 import time
 import sys
 import traceback
+import os
+import json
 from motor import motor_asyncio
 
 client = motor_asyncio.AsyncIOMotorClient()
@@ -20,9 +22,30 @@ headers = ['type','region','time','buyMin','buyAvg','buyMax','buyPercentile','se
           'sellAvg','sellMax','sellPercentile','spread','spread_sma','tradeVolume','volume_sma','velocity',
           'spreadValue','tradeValue']
 
+market_ids = []
+market_id_to_name = {}
+
+with open('../sde/market_groups.js', 'r', encoding='utf-8') as f:
+    read_data = f.read()
+    market_groups_json = read_data
+    market_groups = json.loads(read_data)
+
+    _getItems = lambda items: [x['id'] for x in items]
+
+    def _getGroups(group, ids):
+        if 'items' in group:
+            ids.extend(_getItems(group['items']))
+            for item in group['items']:
+              market_id_to_name[int(item['id'])] = item['name']
+        for _group in group['childGroups']:
+            _getGroups(_group, ids)
+
+    for group in market_groups:
+        _getGroups(group, market_ids)
+
 async def DumpCSV():
 
-  with open('daily_aggregates_%s.csv' % typeid, 'w', newline='') as file:
+  with open('daily_aggregates_%s.csv' % market_id_to_name.get(typeid, typeid), 'w', newline='') as file:
 
     writer = csv.DictWriter(file, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_MINIMAL, fieldnames=headers)
